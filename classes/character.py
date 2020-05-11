c3cdf4d85dbcd78257b3bbcbdb1cb2490893e4d4
Elizabeth Adams <adamsem13@gmail.com>
2020-05-11 19:34:47 -0400

    Added data persistence

--- classes/character.py
+++ classes/character.py
@@ -1,17 +1,12 @@
 class Character:
-    def __init__(self, name, hp, atk):
+    def __init__(self, name, maxhp, hp, atk):
         self.name = name
+        self.maxhp = int(maxhp)
         self.hp = int(hp)    # current health
         self.atk = int(atk)  # attack modifiers
 
-    def get_name(self):
-        return self.name
-
-    def get_hp(self):
-        return str(self.hp)
-
-    def get_atk(self):
-        return str(self.atk)
+    def __str__(self):
+        return self.name + "'s HP is " + str(self.hp) + "/" + str(self.maxhp) + " & ATK is " + str(self.atk)
 
     def take_damage(self, damage):
         self.hp = self.hp - int(damage)
@@ -20,4 +15,6 @@ class Character:
 
     def get_healed(self, healing):
         self.hp = self.hp + int(healing)
+        if self.maxhp < self.hp:
+            self.hp = self.maxhp
 
--- main.py
+++ main.py
@@ -7,14 +7,7 @@ import sys
 import random
 import pandas as pd
 from classes.character import Character
-
-# Initialize
-introText = "RPG Helper \n" + \
-            "A one-stop-shop for dice rolls and tracking character stats.\n\n" + \
-            "Enter 'help' to see usage details\n\n"
-print(introText)
 '''
-    ToDo: Read and write characters to csv file to preserve data between sessions
     Add weapons class
         -attributes: name, damage (ex:3d4+2)
     Add ac (armor class) trait to characters
@@ -22,81 +15,118 @@ print(introText)
 '''
 
 
+# Roll a 4-, 6-, 8-, 10-, 12-, or 20-sided die
 def roll(input_array):
     sides = int(input_array[1])
     possible_sides = set([4, 6, 8, 10, 12, 20])
     if sides in possible_sides:
         print(str(random.randrange(1, sides)))
     else:
-        print("Error: This game is equipped with 4, 6, 8, 10, 12, and 20-sided dice.")
+        print("Error: This game is only equipped with 4, 6, 8, 10, 12, and 20-sided dice.")
 
 
+# Add a character to the character array
 def new(input_array):
     global characters
-    new_char = Character(input_array[1], input_array[2], input_array[3])
-    characters[new_char.get_name()] = new_char
-    print("Huzzah! " + new_char.get_name() + " has joined the game. Welcome to the campaign.")
+    new_char = Character(*input_array[1:])
+    characters[new_char.name] = new_char
+    print("Huzzah! " + new_char.name + " has joined the game. Welcome to the campaign.")
 
 
+# Remove a character from the character array
+def remove(input_array):
+    global characters
+    char = characters.pop(input_array[1], "Character not found")
+    print("Goodbye, " + char.name)
+
+
+# Print character stats for a specified character or for all characters
 def stats(input_array):
     global characters
-    char = characters[input_array[1]]
-    print(char.get_name() + "'s HP is " + char.get_hp() + " & Attack is " + char.get_atk())
+    if input_array[1] == "all":
+        for c in characters:
+            print(characters[c])
+    else:
+        char = characters[input_array[1]]
+        print(char)
 
 
+# Heal character's HP up to max health
 def heal(input_array):
     global characters
     char = characters[input_array[1]]
     char.get_healed(int(input_array[2]))
-    print(char.get_name() + " was healed! New HP is " + char.get_hp())
+    print(char.name + " was healed! New HP is " + char.hp)
 
 
+# Damage character's HP up to 0 (death)
 def damage(input_array):
     global characters
     char = characters[input_array[1]]
     char.take_damage(int(input_array[2]))
     if char.hp == 0:
-        print(char.get_name() + " has perished!")
+        print(char.name + " has perished!")
     else:
-        print(char.get_name() + " took damage! New HP is " + char.get_hp())
+        print(char.name + " took damage! New HP is " + str(char.hp) + "/" + str(char.maxhp))
 
 
-def help(user_input):
-    help_text = "Welcome to the RPG Helper, a command-line app that handles dice rolls and character stats\n" + \
-                "usage:\n" + \
-                "roll [number of dice sides]\n" + \
-                "new [character name] [hp] [atk]\n" + \
-                "stats [character name]\n" + \
-                "heal [character name] [amount]\n" + \
-                "damage [character name] [amount]\n" + \
-                "help\n" + \
-                "quit"
+# Print help text
+def help(user_input = "none"):
+    help_text = "RPG Helper is a command-line app that handles dice rolls and character stats.\n\n" + \
+                "Usage: This app takes a command word followed by parameters as input. The full list of commands " + \
+                "can be found below.\n\n" + \
+                "roll <number_of_dice_sides>                Roll a dice. Ex: To roll a 6-sided die, enter 'roll 6'\n" + \
+                "new <character_name> <maxhp> <hp> <atk>    Add a new character to the campaign.\n" + \
+                "stats <character_name>                     View the stats of the specified character.\n" + \
+                "stats all                                  View the stats of all characters in the campaign.\n" + \
+                "heal <character name> <amount>             Heal a character by a specified amount of HP.\n" + \
+                "damage <character name> <amount>           Damage a character by a specified amount of HP.\n" + \
+                "help                                       Display help text.\n" + \
+                "quit                                       Save to file and quit the session."
     print(help_text)
 
 
-def quit(user_input):
+# Save data and close program
+def quit(user_input = "none"):
+    save()
     sys.exit(0)
 
 
-def save(user_input):
+# Save characters data to .csv file
+def save(user_input = "none"):
     global characters
     column_names = list(vars(list(characters.values())[0]).keys())
     character_attributes = [list(vars(char).values()) for char in characters.values()]
     pd.DataFrame(character_attributes, columns=column_names).to_csv("characters.csv", index=False)
 
 
-def load(user_input):
+# Load data from .csv file
+def load(user_input = "none"):
     global characters
     df = pd.read_csv("characters.csv")
     columns = df.columns.tolist()
-    characters_series = df.apply(lambda x: (x[columns[0]], Character(x[columns[0]], x[columns[1]], x[columns[2]])), axis=1)
+    characters_series = df.apply(lambda x: (x[columns[0]], Character(**dict(x))), axis=1)
     characters = dict(list(characters_series))
     print("Ready players: " + ", ".join(list(characters.keys())))
 
+# Initialize
+introText = "RPG Helper \n" + \
+            "A one-stop-shop for dice rolls and tracking character stats.\n\n" + \
+            "Enter 'help' to see usage details\n\n"
+print(introText)
 
+# Load characters data from file
+characters = []
+load()
+
+# Take user input and execute functions or print error text
 while True:
     try:
         user_input = [user_input for user_input in input().split()]
         getattr(sys.modules[__name__], user_input[0])(user_input)
     except AttributeError:
-        print("Sorry, I didn't recognize that command. Enter 'help' for usage details")
+        print("Sorry, I didn't recognize that command. Please find the list of commands below.\n")
+        help()
+    except IndexError:
+        print("Sorry, I think that command was missing an argument. Please find the list of commands below.\n\n")
+        help()
